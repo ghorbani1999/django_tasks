18fe3f8115b5019b4192e96813aa331bb6118746
ghorbani1999 <morteza.ghorbani@live.com>
2020-03-28 21:38:19 +0430

    finalize the task project

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from django.core.paginator import Paginator
 
 
 def list_tasks(request):
-    paginator = Paginator(models.tasks.objects.all(),4)
+    paginator = Paginator(models.tasks.objects.all(),5)
     current_page = int(request.GET.get('page') or 1 )
     page = paginator.page(current_page)
     tasks_objects = page.object_list
@@ -19,18 +19,35 @@ def list_tasks(request):
     'current_page' : current_page,
     'page':page,
     }
-    return render(request,'list.html',contex)
+    return render(request,'tasks/list.html',contex)
 
 
 def show_task(request,id):
     task = get_object_or_404(models.tasks , id = id)
     contex = {'task':task}
-    return render(request,'show_task.html',contex)
+    return render(request,'tasks/show_task.html',contex)
 
 
 def edit_task(request,id):
-    return render(request,'edit_task.html')
+    task = get_object_or_404(models.tasks , id = id)
+    if request.method == 'GET':
+        contex = {'task':task}
+        return render(request,'tasks/form.html',contex)
+    elif request.method == 'POST':
+        form = taskform(request.POST , instance=task) 
+        if form.is_valid():
+            form.save()
+            messages.success(request,'تغیرات با موفقیت انجام شد')   
+        else:
+            messages.error("validation failed")
+        return redirect('list')    
 
+def delete_task(request , id):
+    if request.method == 'POST':
+        task = get_object_or_404(models.tasks , id = id)
+        task.delete()
+        messages.success(request , 'وظیفه ی مورد نظر حذف شد')
+    return redirect('list')    
 
 def dashboard(request):
     return render(request,'dashboard.html')  
@@ -47,5 +64,5 @@ def create_task(request):
             messages.error(request, 'validation failded')
             return redirect('list')   
     if request.method == 'GET':
-        return render(request,'create_task.html')
+        return render(request,'tasks/form.html')
 # Create your views here.
